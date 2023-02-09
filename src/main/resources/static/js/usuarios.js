01ed13b853be451c6838ad215e9c86e5ab5af68f
Alumno Pedro <alumno.187277@ies-azarquiel.es>
2023-02-09 19:16:42 +0100

    Minuto 1:55. Se carga lista de usuarios en usuarios.html con un for en usuarios.js

--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
 });
 
 async function cargarUsuarios(){
-  const request = await fetch('usuario/123', {
+  const request = await fetch('usuarios', {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
@@ -15,4 +15,11 @@ async function cargarUsuarios(){
   const usuarios = await request.json();
 
   console.log(usuarios);
+
+  let listadoHTML = '';
+  for (let usuario of usuarios){
+    let usuarioHTML = '<tr><td>'+usuario.id+'</td><td>'+usuario.nombre+'</td><td>'+usuario.apellido+'</td><td>'+usuario.email+'</td><td>'+usuario.telefono+'</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>'
+    listadoHTML += usuarioHTML;
+  }
+  document.querySelector('#usuarios tbody').outerHTML = listadoHTML;
 }
