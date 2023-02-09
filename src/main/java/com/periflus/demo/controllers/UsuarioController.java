08ef595b146c8ccf229e34a62920f82dc03a9803
Alumno Pedro <alumno.187277@ies-azarquiel.es>
2023-02-09 19:02:11 +0100

    Minuto 1:45. Funcion getUsuarios en UsuarioController y cargarUsuarios en usuarios.js.

--- src/main/java/com/periflus/demo/controllers/UsuarioController.java
+++ src/main/java/com/periflus/demo/controllers/UsuarioController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 public class UsuarioController {
@@ -17,6 +20,36 @@ public class UsuarioController {
         usuario.setEmail("alumno.187277@ies-azarquiel.es");
         usuario.setTelefono("123123123");
         return usuario;
+    }
+
+    @RequestMapping(value="usuarios")
+    public List<Usuario> getUsuarios(){
+        List<Usuario> usuarios = new ArrayList<>();
+        Usuario usuario = new Usuario();
+        usuario.setId(1L);
+        usuario.setNombre("Pedro");
+        usuario.setApellido("Diaz");
+        usuario.setEmail("alumno.187277@ies-azarquiel.es");
+        usuario.setTelefono("123123123");
+
+        Usuario usuario2 = new Usuario();
+        usuario2.setId(2L);
+        usuario2.setNombre("Raul");
+        usuario2.setApellido("Diaz");
+        usuario2.setEmail("alumno.187277@ies-azarquiel.es");
+        usuario2.setTelefono("123123123");
+
+        Usuario usuario3 = new Usuario();
+        usuario3.setId(3L);
+        usuario3.setNombre("Pablo");
+        usuario3.setApellido("Diaz");
+        usuario3.setEmail("alumno.187277@ies-azarquiel.es");
+        usuario3.setTelefono("123123123");
+
+        usuarios.add(usuario);
+        usuarios.add(usuario2);
+        usuarios.add(usuario3);
+        return usuarios;
     }
 
     @RequestMapping(value="editar")
--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -1,8 +1,18 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
+  cargarUsuarios();
   $('#usuarios').DataTable();
 });
 
-function cargarUsuarios(){
+async function cargarUsuarios(){
+  const request = await fetch('usuario/123', {
+    method: 'GET',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    }
+  });
+  const usuarios = await request.json();
 
+  console.log(usuarios);
 }
