c6d77dfe0e17b4a5c8ddacba14001a35a569c741
Alumno Pedro <alumno.187277@ies-azarquiel.es>
2023-02-09 18:14:14 +0100

    Minuto 1:24. Primer getUsuario pasando ID y creación de entidad Usuario.

--- src/main/java/com/periflus/demo/controllers/UsuarioController.java
+++ src/main/java/com/periflus/demo/controllers/UsuarioController.java
@@ -1,13 +1,41 @@
 package com.periflus.demo.controllers;
 
+import com.periflus.demo.models.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
 public class UsuarioController {
-    @RequestMapping(value="prueba")
-    public String prueba(){
-        return "prueba";
+    @RequestMapping(value="usuario/{id}")
+    public Usuario getUsuario(@PathVariable Long id){
+        Usuario usuario = new Usuario();
+        usuario.setId(id);
+        usuario.setNombre("Pedro");
+        usuario.setApellido("Diaz");
+        usuario.setEmail("alumno.187277@ies-azarquiel.es");
+        usuario.setTelefono("123123123");
+        return usuario;
+    }
+
+    @RequestMapping(value="editar")
+    public Usuario editar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Pedro");
+        usuario.setApellido("Diaz");
+        usuario.setEmail("alumno.187277@ies-azarquiel.es");
+        usuario.setTelefono("123123123");
+        return usuario;
+    }
+
+    @RequestMapping(value="eliminar")
+    public Usuario eliminar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Pedro");
+        usuario.setApellido("Diaz");
+        usuario.setEmail("alumno.187277@ies-azarquiel.es");
+        usuario.setTelefono("123123123");
+        return usuario;
     }
 }
